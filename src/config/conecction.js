1a447000444e62a7ef083ebab03dad5bc4d33344
yuri.andrade <yuri.andarde@dbserver.com.br>
2021-09-25 16:25:22 -0300

    correções, e adição da biblioteca dotenv

--- src/config/conecction.js
+++ src/config/conecction.js
@@ -2,10 +2,10 @@
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host : 'localhost',
-    user : 'postgres',
-    password : '102323',
-    database : 'AINDA_VOU_CRIAR'
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_DATABASE
   }
 });
 
