514d1765e47a097beb099e3638501b619d1ae5b4
yuri.andrade <yuri.andarde@dbserver.com.br>
2021-09-26 22:02:06 -0300

    rota cadastrar usuario, parcialmente criada

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -10,10 +10,28 @@ const registerUser = async (req, res) => {
   } = req.body;
 
   try {
+    //validando dados enviados pelo usuário
     await schemaRegisterUser.validate(req.body);
 
-    res.json('validou ;)');
+    const verifyIfEmailAlreadyExists = await knex('usuarios').select('*').where('email', email);
 
+    if (verifyIfEmailAlreadyExists.length > 0) {
+      return res.status(400).json('E-mail já cadastrado.');
+    };
+
+    const encryptedPassword = await bcrypt.hash(senha, 10);
+
+    const registeringUser = await knex('usuarios').insert({
+      nome,
+      email,
+      senha: encryptedPassword
+    })
+
+    if (registeringUser.rowCount < 1) {
+      return res.status(400).json('O usuário não foi cadastrado.');
+    }
+
+    return res.status(200).json('Conta criada com sucesso!');
   } catch (error) {
     return res.status(400).json(error.message);
   }
