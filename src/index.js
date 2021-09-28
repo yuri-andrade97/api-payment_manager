10e1ae785d04b3548763d96fe28ec1a1c1d615b0
yuri.andrade <yuri.andarde@dbserver.com.br>
2021-09-28 19:22:02 -0300

    att

--- src/index.js
+++ src/index.js
@@ -10,6 +10,4 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('API Started ;)');
-})
+app.listen(process.env.PORT || 3000)
