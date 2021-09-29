111c3918856f9cca81b166322349ba62d349b5a5
yuri.andrade <yuri.andarde@dbserver.com.br>
2021-09-28 21:24:29 -0300

    att

--- src/config/conecction.js
+++ src/config/conecction.js
@@ -1,6 +1,6 @@
 const knex = require('knex')({
   client: 'pg',
-  connection: {
+  connection: process.env.DATABASE_URL || {
     host : process.env.DB_HOST,
     user : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
