763d354ce666fac0dad3c9a19edcc2ff2806b8b2
yuri.andrade <yuri.andarde@dbserver.com.br>
2021-09-29 21:24:42 -0300

    teste

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -12,6 +12,9 @@ const registerUser = async (req, res) => {
     senha
   } = req.body;
 
+
+  return res.json(senha)
+
   try {
     //validando dados enviados pelo usuário
     await schemaRegisterUser.validate(req.body);
