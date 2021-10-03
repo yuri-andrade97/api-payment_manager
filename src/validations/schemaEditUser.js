cd8d2663e2c56b1e1ba76114c5cbaab10bbfbbf2
yuri.andrade <yuri.andarde@dbserver.com.br>
2021-10-03 09:22:58 -0300

    teste

--- src/validations/schemaEditUser.js
+++ src/validations/schemaEditUser.js
@@ -3,7 +3,7 @@ const yup = require('./settings');
 const schemaEditUser = yup.object().shape({
   nome: yup.string().required(),
   email: yup.string().email().required(),
-  senha: yup.string().strict().required(),
+  senha: yup.string().strict(),
   cpf: yup.string().min(11),
   telefone: yup.string()
 })
