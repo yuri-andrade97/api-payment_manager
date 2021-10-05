199d435dd63094d3b3bf35d8e38baf7ae83fb72c
yuri.andrade <yuri.andarde@dbserver.com.br>
2021-10-05 20:35:36 -0300

    att

--- src/middlewares/authToken.js
+++ src/middlewares/authToken.js
@@ -10,6 +10,7 @@ const authToken = async (req, res, next) => {
 
   try {
     const token = authorization.replace('Bearer ', '').trim();
+    console.log(token)
 
     const dataUser = jwt.verify(token, process.env.JWT_TOKEN);
 
